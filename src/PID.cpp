e456c067788af11fea9feb96cd1ec71ff1971fcb
jamestouri <j_touri@yahoo.com>
2018-01-06 22:01:52 -0800

    Continuing init

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <vector>
 
 using namespace std;
 
@@ -14,6 +15,10 @@ void PID::Init(double Kp, double Ki, double Kd) {
     int n = 100;
     float speed = 20.0;
     
+    vector<float> x_trajectory;
+    vector<float> y_trajectory;
+    
+    int int_cte = 0;
     
 }
 
